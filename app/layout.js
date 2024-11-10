fad8fbb897a4fc32175e5a4ff733cf402eae7218
Aryan shinde <shindearyan179@gmail.com>
2024-11-10 12:57:31 +0530

    Added Google Adsense Meta Tag

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <head>
+        {/* Adsense */}
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-1591708379540743"
+        ></meta>
+
+        {/* Google Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link
           rel="preconnect"
@@ -23,8 +30,8 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
           rel="stylesheet"
         ></link>
-        {/* Favicon */}
 
+        {/* Favicon */}
         <link
           rel="apple-touch-icon"
           sizes="57x57"
